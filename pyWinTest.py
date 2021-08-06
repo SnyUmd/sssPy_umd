2711e6e8e709f97ed6e519ab08163bab0e8f48df
SnyUmd <sny.umd@gmail.com>
2021-08-07 01:16:41 +0900

    a

---
+++ pyWinTest.py
@@ -0,0 +1,23 @@
+# coding: UTF-8
+import tkinter
+from tkinter import filedialog
+
+def btn_Click():
+    typ = [('Excelファイル','*.xlsx')]
+    dir = 'D:\_git\Python'
+    fle = filedialog.askopenfilename(filetypes = typ, initialdir = dir)
+    print(fle)
+
+
+# Tkクラス生成
+frm = tkinter.Tk()
+# 画面サイズ
+frm.geometry('200x100')
+# 画面タイトル
+frm.title('サンプル画面')
+
+btn = tkinter.Button(frm, text='ボタン', width=10, height=1, command=btn_Click )
+btn.place(x=100, y=10)
+
+# 画面をそのまま表示
+frm.mainloop()
